fe504d9025d887c1f17c8f1601016dc3d8d0870c
Narokan <nick.kirichenko.dev@gmail.com>
2023-07-03 23:25:31 +0300

    requirements update 4

--- ingotcrm/settings.py
+++ ingotcrm/settings.py
@@ -8,7 +8,7 @@ env = environ.Env(
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-READ_DOT_ENV_FILE = env.bool('READ_DOT_ENV_FILE', default=False) # False in production
+READ_DOT_ENV_FILE = env.bool('READ_DOT_ENV_FILE', default=True) # False in production
 
 if READ_DOT_ENV_FILE:
     environ.Env.read_env(os.path.join(BASE_DIR / "ingotcrm", '.env'))
@@ -16,7 +16,7 @@ if READ_DOT_ENV_FILE:
 DEBUG = env('DEBUG')
 SECRET_KEY = env('SECRET_KEY')
 
-ALLOWED_HOSTS = ['*', '.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app']
 
 # Application definition
 
